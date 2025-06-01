ef9148ea7bc3072c248f93ee930ed5e2082a658c
codehasril <harshilRana504@gmail.com>
2025-06-01 17:48:10 +0530

    make some change

--- NoteNow/src/Componets/NoteList.jsx
+++ NoteNow/src/Componets/NoteList.jsx
@@ -4,7 +4,7 @@ import AddNote from './AddNote'
 
 const NoteList = ({Notes , setNotes , handleAddnote , handledeleteNote , dark,setdark}) => {
   return (
-    <div className={`grid gap-4 grid-cols-[repeat(auto-fit,minmax(330px,1fr))] ${dark ? 'bg-black text-white' : 'bg-white text-black'}`}>
+    <div className={`grid gap-4 grid-cols-[repeat(auto-fit,minmax(330px,1fr))] ${dark ? 'bg-black text-white' : 'bg-white text-black'}`} >
 
       {Notes.map((note) => (
          <Note  id={note.id} text={note.text} date={note.date}  handledeleteNote={handledeleteNote} />
